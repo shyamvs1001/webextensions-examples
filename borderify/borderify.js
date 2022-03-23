9b6963531b7fc80aee96602bfa64daed4b0746ba
shyamvs1001 <shyamvs1001@gmail.com>
2022-03-23 15:59:21 +0530

    Hide non results

--- borderify/borderify.js
+++ borderify/borderify.js
@@ -1,4 +1,5 @@
 /*
 Just draw a border round the document.body.
 */
-document.body.style.border = "5px solid red";
+//document.body.style.border = "5px solid red";
+jss.set('.search-term-absent', {'display':'none'});
